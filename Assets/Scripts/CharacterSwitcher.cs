ecad6b39fa71efe8a59691508404864f54abfda2
Fabian Schwarz <fabian12943@gmail.com>
2021-10-16 13:11:52 +0200

    Add local multiplayer functionality

---
+++ Assets/Scripts/CharacterSwitcher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterSwitcher : MonoBehaviour
+{
+    int index = 0;
+    [SerializeField] List<GameObject> players = new List<GameObject>();
+    PlayerInputManager manager;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        manager = GetComponent<PlayerInputManager>();
+        index += 1;
+    }
+
+    public void SwitchNextSpawnCharacter(PlayerInput input) 
+    {
+        manager.playerPrefab = players[index % players.Count];
+        index += 1;
+    }
+}
