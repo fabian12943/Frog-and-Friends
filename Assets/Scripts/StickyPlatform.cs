337ca123b7ddfd49a9c1576db5438f6bf074ae92
Lyrics17 <57186088+Lyrics17@users.noreply.github.com>
2021-11-24 17:13:36 +0100

    Created Moving Platfom Prefab & StickyPlatform Script
    Sticky means the player can stand on the platform without being pushed down

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("(Clone)"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("(Clone)")) //TODO: Aks Fabian how Characters are created
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
