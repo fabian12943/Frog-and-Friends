8434be53831375600459e1ce332be346f44f0b8e
Fabian Schwarz <fabian12943@gmail.com>
2021-12-20 17:35:14 +0100

    Adjust WaypointFollower script to move through given waypoints on the way back

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -6,16 +6,30 @@ public class WaypointFollower : MonoBehaviour
 {
     [SerializeField] private GameObject[] waypoints;
     private int currentWaypointIndex = 0;
+    private bool onReturn = false;
 
     [SerializeField] private float speed = 2f;
     private void Update()
     {
         if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
         {
-            currentWaypointIndex++;
-            if (currentWaypointIndex >= waypoints.Length)
+            if (!onReturn) 
             {
-                currentWaypointIndex = 0;
+                currentWaypointIndex++;
+                if (currentWaypointIndex >= waypoints.Length) 
+                {
+                    currentWaypointIndex = waypoints.Length - 1;
+                    onReturn = true;
+                }
+            }
+            else 
+            {
+                currentWaypointIndex--;
+                if (currentWaypointIndex < 0) 
+                {
+                    currentWaypointIndex = 0;
+                    onReturn = false;
+                }
             }
         }
         transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
