aad37566110a12023ff7f808c7434175f856188e
Lyrics17 <57186088+Lyrics17@users.noreply.github.com>
2021-12-21 19:34:36 +0100

    Added Apple & Pineapple Prefab, implemented state switch script for collectables and restructured ItemCollector for Player gameObject
    StateSwitchCollectable Scripts triggers collected animations if gameObjects collides with Player Object
    !!! ItemCollector Script does not work with PlayerManager!!! Needs Fix

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -5,58 +5,29 @@ using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-
-    private Animator anim;
-    private enum ItemState { idle, collected, destroyed }
-
     private int points = 0;
 
     [SerializeField] private Text pointsText;
 
-    private void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        anim = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-        if (AnimatorIsPlaying("Collected"))
+        if (collision.gameObject.CompareTag("Collectable"))
         {
-            if (name.Contains("Apple"))
+           if (collision.name.Contains("Apple"))
             {
                 points++;
             }
 
-            if (name.Contains("Cherry"))
+            if (collision.name.Contains("Cherry"))
             {
                 points = points + 2;
             }
 
-            if (name.Contains("Pineapple"))
+            if (collision.name.Contains("Pineapple"))
             {
                 points += 5;
             }
             pointsText.text = "Points: " + points;
-            Destroy(gameObject);
         }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
-        ItemState state = ItemState.idle;
-
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            state = ItemState.collected;
-        }
-
-        anim.SetInteger("state", (int)state);
-    }
-    
-    //TODO: Put Script into player maybe 
-
-    bool AnimatorIsPlaying(string stateName)
-    {
-        return anim.GetCurrentAnimatorStateInfo(0).IsName(stateName);
-    }
+    }   
 }
