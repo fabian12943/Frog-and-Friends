e40aed866af2594e03363a7163b05ee96862e520
Lyrics17 <57186088+Lyrics17@users.noreply.github.com>
2021-12-21 17:39:48 +0100

    Implemented Timer Script and added Display Text of Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float timeRemaining = 6; //==> 10Min Gametime
+    private bool timerIsRunning = false;
+
+    [SerializeField] private Text timeText;
+
+
+    private void Start()
     {
-        
+        // Starts the timer automatically
+        timerIsRunning = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                DisplayTime(timeRemaining);
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                RestartLevel();
+            }
+        }
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
