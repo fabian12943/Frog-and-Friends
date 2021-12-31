2585a151d7b4ddb0b620e5574ffc1264e9c65479
Lyrics17 <57186088+Lyrics17@users.noreply.github.com>
2021-12-31 17:27:49 +0100

    Added PlantEnemy to SampleScene, added "Deadly" tag to Bullet Object
    TODO: fix flip problem of PlantEnemy

--- Assets/Scripts/BulletLogic.cs
+++ Assets/Scripts/BulletLogic.cs
@@ -10,14 +10,11 @@ public class BulletLogic : MonoBehaviour
     [SerializeField] private float shotDistance;
 
     private Vector2 target;
-    // private Vector2 plantMouth;
 
     // Start is called before the first frame update
     void Start()
     {
-        // plantMouth = new Vector2(transform.position.x + 1.12f, transform.position.y + 0.2f); //TODO: muss noch richtig angepasst werden nervig
         target = new Vector2(shotDistance, transform.position.y);
-        // transform.position = new Vector2(transform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
@@ -34,6 +31,4 @@ public class BulletLogic : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    // TODO: deadly tag 
 }
