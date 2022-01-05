aeb2f3ce767cfc14b22d7630b0d58beafa3741e2
Fabian Schwarz <fabian12943@gmail.com>
2022-01-05 18:50:40 +0100

    Add missing players message at beginning of game

---
+++ Assets/JoinPlayerMessageController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class JoinPlayerMessageController : MonoBehaviour
+{
+    TextMeshProUGUI textMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetChildWithName(gameObject, "Remaining Players").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int spawnedPlayersAmount = GameObject.FindGameObjectsWithTag("Player").Length;
+        int playersAmountTotal = PlayerPrefs.GetInt("noOfPlayers", 1);
+        if (spawnedPlayersAmount == playersAmountTotal)
+        {
+            gameObject.SetActive(false);
+        }
+        else 
+        {
+            gameObject.SetActive(true);
+        }
+
+        textMesh.text = "Waiting for " + (playersAmountTotal - spawnedPlayersAmount) + " more player(s).";
+    }
+
+    private GameObject GetChildWithName(GameObject obj, string name)
+    {
+        Transform trans = obj.transform;
+        Transform childTrans = trans.Find(name);
+        if (childTrans != null) 
+        {
+            return childTrans.gameObject;
+        } 
+        else 
+        {
+            return null;
+        }
+    }
+}
--- Assets/MenuButtonFunctions.cs
+++ Assets/MenuButtonFunctions.cs
@@ -97,6 +97,6 @@ public class MenuButtonFunctions : MonoBehaviour
         {
             return null;
         }
- }
+    }
 
 }
