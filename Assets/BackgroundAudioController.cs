8e8d84a95b24524d0227e431ea8b8dffb562473b
Fabian Schwarz <fabian12943@gmail.com>
2022-01-07 13:39:35 +0100

    Add background music to menu

---
+++ Assets/BackgroundAudioController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudioController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] backgroundSongs;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        audioSource = GetComponent<AudioSource>();
+
+        // Set volume
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+
+        AudioListener.volume = PlayerPrefs.GetFloat("musicVolume");
+
+        // Play random song
+        AudioSource audio = GetComponent<AudioSource>();
+        if (!audioSource.isPlaying)
+        {
+            audio.clip = backgroundSongs[Random.Range(0, backgroundSongs.Length)];
+            audio.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = backgroundSongs[Random.Range(0, backgroundSongs.Length)];
+            audioSource.Play();
+        }
+    }
+
+}
