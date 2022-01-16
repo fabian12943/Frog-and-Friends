fe24fbc364208b772e1716d62ae9eafe296ec7d1
Fabian Schwarz <fabian12943@gmail.com>
2022-01-16 16:13:51 +0100

    Fix bug that made rhino move through walls

--- Assets/Scripts/RhinoMovement.cs
+++ Assets/Scripts/RhinoMovement.cs
@@ -6,7 +6,7 @@ public class RhinoMovement : MonoBehaviour
 {   
     public float mMovementSpeed = 5.0f;
     public bool bIsGoingRight = true;
-    public float mRaycastingDistance = 1.5f;
+    public float mRaycastingDistance = 0.1f;
 
     private SpriteRenderer _mSpriteRenderer;
     private Animator animator;
@@ -26,14 +26,14 @@ public class RhinoMovement : MonoBehaviour
     {
         if (mustPatrol)
         {
-            animator.SetInteger("state", 1);
-            // if the ennemy is going right, get the vector pointing to its right
-            Vector3 directionTranslation = (bIsGoingRight) ? transform.right : -transform.right; 
-            directionTranslation *= Time.deltaTime * mMovementSpeed;
+            if (!CheckForWalls()){
+                animator.SetInteger("state", 1);
+                // if the ennemy is going right, get the vector pointing to its right
+                Vector3 directionTranslation = (bIsGoingRight) ? transform.right : -transform.right; 
+                directionTranslation *= Time.deltaTime * mMovementSpeed;
 
-            transform.Translate(directionTranslation);
-
-            CheckForWalls();
+                transform.Translate(directionTranslation);
+            }
         }
         else
         {
@@ -41,23 +41,28 @@ public class RhinoMovement : MonoBehaviour
         }
     }
 
-    private void CheckForWalls()
+    private bool CheckForWalls()
     {
         Vector3 raycastDirection = (bIsGoingRight) ? Vector3.right : Vector3.left;
 
         // Raycasting takes as parameters a Vector3 which is the point of origin, another Vector3 which gives the direction, and finally a float for the maximum distance of the raycast
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
 
         // if we hit something, check its tag and act accordingly
-        if (hit.collider != null)
+        if (hits.Length != 0)
         {
-            if (hit.transform.tag == "Terrain")
+            foreach (RaycastHit2D hit in hits)
             {
-                mustPatrol = false;
-                animator.Play("Rhino_Hit_Wall");
-                StartCoroutine(Flip());
+                if (hit.transform.tag == "Terrain")
+                {
+                    mustPatrol = false;
+                    animator.Play("Rhino_Hit_Wall");
+                    StartCoroutine(Flip());
+                    return true;
+                }
             }
         }
+        return false;
     }
 
     IEnumerator Flip()
